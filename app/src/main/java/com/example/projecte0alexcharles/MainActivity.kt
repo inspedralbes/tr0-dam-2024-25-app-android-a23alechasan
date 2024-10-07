d29dac8246aa92b68b4d354f82a9e5b0de95f177
a23alechasan <a23alechasan@inspedralbes.cat>
2024-10-07 09:11:44 +0200

    Primera pujada

---
+++ app/src/main/java/com/example/projecte0alexcharles/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.projecte0alexcharles
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.projecte0alexcharles.ui.theme.Projecte0AlexCharlesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Projecte0AlexCharlesTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Projecte0AlexCharlesTheme {
+        Greeting("Android")
+    }
+}
